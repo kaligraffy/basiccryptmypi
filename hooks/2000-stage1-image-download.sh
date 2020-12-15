6d6bbe77addb5c0f074a3927ed3321a81a2a870f
kaligraffy <kaligraffy@protonmail.com>
2020-12-15 15:45:25 +0000

    changes to download/extract and cpu-governor using package already in kali repo

--- hooks/2000-stage1-image-download.sh
+++ hooks/2000-stage1-image-download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-echo_info "Starting download at $(date)"
 IMAGEFILE=${_IMAGEDIR}/${_IMAGENAME}
-#wget -nc "${_IMAGEURL}" -O "${IMAGEFILE}" #TESTING
-echo "$_IMAGESHA"  $IMAGEFILE | sha256sum --check --status
-#rm ${_IMAGEDIR}/${_IMAGENAME} #TESTING
-echo_debug "Image valid"
+echo_info "Starting download at $(date)"
+wget -nc "${_IMAGEURL}" -O "${IMAGEFILE}" || true
 echo_info "Completed download at $(date)"
+echo_info "Checking image checksum"
+echo ${_IMAGESHA}  $IMAGEFILE | sha256sum --check --status
+echo_info "- valid"
--- hooks/2100-stage1-image-extract.sh
+++ hooks/2100-stage1-image-extract.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 set -e
 
-mkdir ${_BUILDDIR}/root
-mkdir ${_BUILDDIR}/mount
+IMAGE="${_IMAGEDIR}/${_IMAGENAME}"
+EXTRACTEDIMAGE="${_IMAGEDIR}/extracted.img"
+
+mkdir $_BUILDDIR/mount
 
 echo_info "Starting extract at $(date)"
-echo_debug "Extracting image: ${_IMAGENAME}"
-if [ ! -e ${_IMAGEDIR}/cryptmypi.img ]; then
-    case ${_IMAGENAME} in
+if [ -e "$EXTRACTEDIMAGE" ]; then
+    echo_info "$EXTRACTEDIMAGE found"
+else
+    case ${IMAGE} in
         *.xz)
-            xz --decompress --stdout ${_IMAGEDIR}/${_IMAGENAME} > ${_IMAGEDIR}/cryptmypi.img
+            echo_info "Extracting with xz"
+            xz --decompress --stdout ${IMAGE} > $EXTRACTEDIMAGE
             ;;
         *.zip)
-            unzip -p ${_IMAGEDIR}/${_IMAGENAME} > ${_IMAGEDIR}/cryptmypi.img
+            echo_info "Extracting with unzip"
+            unzip -p $IMAGE > $EXTRACTEDIMAGE
             ;;
         *)
-            echo_error "Unknown extension type on image: ${_IMAGENAME}"
+            echo_error "Unknown extension type on image: $IMAGE"
             exit 1
             ;;
     esac
-else 
-    echo_debug "${_IMAGEDIR}/cryptmypi.img found, skipping extract"
 fi
+
+# At this point, there is a a decompressed image in the IMAGE/folder
+# Now we
+
 echo_info "Finished extract at $(date)"
+exit
 echo_debug "Mounting loopback ..."
-loopdev=$(losetup -P -f --show ${_IMAGEDIR}/cryptmypi.img)
+loopdev=$(losetup -P -f --show "$EXTRACTEDIMAGE")
 
+echo_debug "Unmounted $_BUILDDIR/boot $(date)"
 mount ${loopdev}p2 ${_BUILDDIR}/mount/
-echo_info "Starting copy of root to ${_BUILDDIR}/root $(date)"
-cp -a ${_BUILDDIR}/mount/* ${_BUILDDIR}/root/
-umount ${_BUILDDIR}/mount
-echo_info "Finished copy of root to ${_BUILDDIR}/root $(date)"
-
 mount ${loopdev}p1 ${_BUILDDIR}/mount/boot
-echo_info "Starting copy of boot to ${_BUILDDIR}/boot $(date)"
-cp -a ${_BUILDDIR}/mount/* ${_BUILDDIR}/root/boot/
-umount ${_BUILDDIR}/mount/boot
-echo_info "Finished copy of root to ${_BUILDDIR}/root $(date)"
 
+echo_info "Starting copy of boot to ${_BUILDDIR}/boot $(date)"
+cp -a ${_BUILDDIR}/mount ${_BUILDDIR}/root
+echo_info "Finished copy of boot to ${_BUILDDIR}/boot $(date)"
+
+echo_debug "Unmounted ${_BUILDDIR}/boot $(date)"
+umount ${_BUILDDIR}/mount/boot
+umount ${_BUILDDIR}/mount
+rmdir ${_BUILDDIR}/mount
 echo_debug "Cleaning loopback ..."
 losetup -d ${loopdev}
