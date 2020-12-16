7d4bd169728eda409ebb8bba82af2fe9a0be4b5a
kaligraffy <kaligraffy@protonmail.com>
2020-12-16 16:48:15 +0000

    Added dependencies file

---
+++ dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get -qq install \
+qemu-user-static \
+binfmt-support \
+coreutils \
+parted \
+qemu-user-static \
+debootstrap  \
+zip \ 
+grep \
+rsync \
+xz-utils \
+btrfs-progs
